c309191592aae53e7d7b739bb968fe76323f3a36
Emily <lam.emily813@gmail.com>
2020-05-13 12:08:40 -0400

    Adds button block style for blue outlines

--- themes/AKGShelter/assets/js/block-editor.js
+++ themes/AKGShelter/assets/js/block-editor.js
@@ -1,1 +1,12 @@
-console.log('AKGShelter js Ready!')
+console.log('AKGShelter js Ready!');
+
+wp.domReady( function() {
+    // only needs to be here to unregister blocks
+    
+    // wp.blocks.unregisterBlockStyle( 'core/quote', 'large' );
+
+    wp.blocks.registerBlockStyle( 'core/heading', {
+        name: 'underline',
+        label: 'Underline',
+    } );
+} );
---
+++ themes/AKGShelter/inc/block-editor.php
@@ -0,0 +1,8 @@
+<?php
+register_block_style(
+    'core/button',
+    array(
+        'name'         => 'blue-outline-button',
+        'label'        => __( 'Blue Outline Button' ),
+    )
+);
