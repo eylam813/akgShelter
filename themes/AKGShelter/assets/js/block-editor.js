002252ccbce38e24643189ba03d79500546f9144
Emily <lam.emily813@gmail.com>
2020-05-13 15:31:13 -0400

    Unregister outline style on button, add hover-state to blue-outline-button

--- themes/AKGShelter/assets/js/block-editor.js
+++ themes/AKGShelter/assets/js/block-editor.js
@@ -3,10 +3,6 @@ console.log('AKGShelter js Ready!');
 wp.domReady( function() {
     // only needs to be here to unregister blocks
     
-    // wp.blocks.unregisterBlockStyle( 'core/quote', 'large' );
-
-    wp.blocks.registerBlockStyle( 'core/heading', {
-        name: 'underline',
-        label: 'Underline',
-    } );
+    wp.blocks.unregisterBlockStyle( 'core/button', 'outline' );
+    
 } );
